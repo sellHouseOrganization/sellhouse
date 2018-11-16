a39093a206eddc8f0711fd4bfc15c796f7e7b9f6
huangwei18374836613 <huangweiaa1992@qq.com>
2018-11-16 15:45:57 +0800

    application 第一次提交

--- src/main/java/com/newer/sellhouse/Application.java
+++ src/main/java/com/newer/sellhouse/Application.java
@@ -1,12 +1,14 @@
 package com.newer.sellhouse;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SellhouseApplication {
+@MapperScan()
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(SellhouseApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 }
